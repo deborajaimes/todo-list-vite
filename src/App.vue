1e32d3b343cbc0369c197c63e60b12f2dc6f7372
Debora Jaimes <deboranjaimes@gmail.com>
2021-10-09 17:57:23 -0400

    Primeiro Commit com Vue

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <h1 class="text-6xl text-center mt-28">{{ titulo }}</h1>
+  <div
+    class="
+      space-y-4
+      border-2 border-gray-300
+      w-1/3
+      rounded-sm
+      p-8
+      mx-auto
+      mt-20
+    "
+  >
+    <div>
+      <label for="descricao" class="block">Descrição: </label>
+      <input
+        v-model="tarefa.descricao"
+        type="text"
+        id="descricao"
+        placeholder="Digite a descrição da tarefa"
+        class="
+          border-2 border-gray-300
+          outline-none
+          rounded-sm
+          px-4
+          py-2
+          focus:border-blue-600
+          w-full
+        "
+      />
+    </div>
+    <div>
+      <label for="data" class="block">Data: </label>
+      <input
+        v-model="tarefa.data"
+        type="date"
+        id="data"
+        class="
+          border-2 border-gray-300
+          outline-none
+          rounded-sm
+          px-4
+          py-2
+          focus:border-blue-600
+          w-full
+        "
+      />
+    </div>
+    <app-btn class="mx-auto" @click="salvar">Salvar</app-btn>
+  </div>
+  <div  class="w-1/3 mx-auto border-2 boder-gray-300 mt-18">
+    <tabela-tarefas :tarefas="tarefas"></tabela-tarefas>
+  </div>
+</template>
+
+<script>
+import TabelaTarefas from "./components/TabelaTarefas.vue";
+import AppBtn from "./components/AppBtn.vue";
+
+export default {
+  components: {
+    TabelaTarefas,
+    AppBtn,
+  },
+  data() {
+    return {
+      titulo: "TodoList Vite",
+      tarefa: {},
+      tarefas: [],
+    };
+  },
+  methods: {
+    salvar() {
+      this.tarefas.push(Object.assign({}, this.tarefa));
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/TabelaTarefas.vue
@@ -0,0 +1,46 @@
+<template>
+  <table class="w-full text-md bg-white rounded-md mb-4">
+    <tbody>
+      <tr class="border-b">
+        <th class="text-left p-3 px-5">Descrição</th>
+        <th class="text-left p-3 px-5">Data</th>
+        <th class="text-left p-3 px-5">Concluída</th>
+        <th></th>
+      </tr>
+      <tr
+        v-for="(item, i) in tarefas"
+        :key="item.descricao"
+        :class="[
+          { riscado: item.concluida == true },
+          { 'bg-gray-100': i % 2 == 0 },
+          'border-b hover:bg-gray-300',
+        ]"
+      >
+        <td class="p-3 px-5">{{ item.descricao }}</td>
+        <td class="p-3 px-5">{{ formatarData(item.data) }}</td>
+        <td class="p-3 px-5">
+          <input v-model="item.concluida" type="checkbox" />
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import moment from "moment";
+
+export default {
+  props: ["tarefas"],
+  methods: {
+    formatarData(data) {
+      return moment(data).format("DD/MM/yyyy");
+    },
+  },
+};
+</script>
+
+<style>
+.riscado {
+  text-decoration: line-through;
+}
+</style>
